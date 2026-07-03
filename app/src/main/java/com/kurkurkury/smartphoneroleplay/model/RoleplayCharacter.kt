7c101fe08b6b04f0a4070d4e0d19b1ef7ae33e62
Kurkurkury <hallowelt1432@gmx.ch>
2026-07-03 16:17:04 +0200

    Add roleplay character model

---
+++ app/src/main/java/com/kurkurkury/smartphoneroleplay/model/RoleplayCharacter.kt
@@ -0,0 +1,9 @@
+package com.kurkurkury.smartphoneroleplay.model
+
+data class RoleplayCharacter(
+    val id: String,
+    val name: String,
+    val description: String,
+    val greeting: String,
+    val personality: String
+)
